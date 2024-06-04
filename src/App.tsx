22b3e578cb673e132ac0e150d97869ab03d7ff07
Benjamin Ng <60500272+benymng@users.noreply.github.com>
2024-06-04 11:54:28 -0400

    Add basic routing and styling library

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Purchase } from "./pages/Purchase";
 import "./App.css";
 
 function App() {
-  return <>hi</>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="purchase" element={<Purchase />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    ,
+    "./node_modules/preline/preline.js",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("preline/plugin")],
 };
