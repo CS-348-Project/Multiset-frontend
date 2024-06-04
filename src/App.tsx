f1b399d50c6f55bce22dd758e15a4970d1b1be68
Benjamin Ng <60500272+benymng@users.noreply.github.com>
2024-06-04 13:52:53 -0400

    Revert "Add basic routing and styling library"
    This reverts commit 22b3e578cb673e132ac0e150d97869ab03d7ff07.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,7 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Home } from "./pages/Home";
-import { Purchase } from "./pages/Purchase";
 import "./App.css";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="purchase" element={<Purchase />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <>hi</>;
 }
 
 export default App;
--- src/pages/Purchase.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const Purchase = () => {
-  return <div>Purhcase</div>;
-};
