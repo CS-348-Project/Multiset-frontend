31d478663432e4b8a38a5521320c174b486cae06
Kenneth Ruan <kennethjruan@gmail.com>
2024-06-04 17:51:21 -0400

    Initial react router setup

---
+++ src/routes/root.tsx
@@ -0,0 +1,11 @@
+const Root = () => {
+  return (
+    <div className="w-screen h-screen bg-orange-50">
+      <div className="w-full h-full flex items-center justify-center">
+        <div className="text-4xl font-bold text-black"> Multiset </div>
+      </div>
+    </div>
+  );
+};
+
+export default Root;
