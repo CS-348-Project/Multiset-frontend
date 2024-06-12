ac77384b178bd6ce3937a66c9a331458ae64d06c
Benjamin Ng <60500272+benymng@users.noreply.github.com>
2024-06-12 18:21:37 -0400

    Add google auth

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import { Toaster } from "./components/ui/toaster";
 import { Purchase } from "./routes/purchase";
 import { Settlement } from "./routes/settlement";
 import { Analytics } from "./routes/analytics";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const router = createBrowserRouter([
   {
@@ -38,9 +39,11 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <main className="font-sans">
-      <RouterProvider router={router} />
-      <Toaster />
-    </main>
+    <GoogleOAuthProvider clientId="351079269408-cjr2v77v05pb1f6pqj1f3cb8v93k0s4t.apps.googleusercontent.com">
+      <main className="font-sans">
+        <RouterProvider router={router} />
+        <Toaster />
+      </main>
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const API_BASE_URL = import.meta.env.VITE_API_URL;
+const token = localStorage.getItem("token");
 
 export const apiService = axios.create({
   baseURL: API_BASE_URL,
+  headers: {
+    Authorization: `Bearer ${token}`,
+  },
 });
