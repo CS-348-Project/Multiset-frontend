859cafdddd5cf4be78691c77ee5f43399810c0d1
Benjamin Ng <60500272+benymng@users.noreply.github.com>
2024-06-12 20:26:49 -0400

    Add logout functionality

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,13 +1,29 @@
 import { apiService } from "@/utils/api";
 import { GoogleLogin } from "@react-oauth/google";
+import { useEffect, useState } from "react";
 
 const Root = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    setIsLoggedIn(false);
+  };
+
   const handleLogin = (response) => {
     console.log(response);
     apiService
       .post("api/auth/google", { token: response.credential })
       .then((res) => {
         localStorage.setItem("token", res.data.token);
+        setIsLoggedIn(true);
       })
       .catch((err) => {
         console.error(err);
@@ -16,14 +32,20 @@ const Root = () => {
   return (
     <div className="w-screen h-screen bg-orange-50">
       <div className="w-full h-full flex items-center justify-center">
-        <GoogleLogin
-          onSuccess={(credentialResponse) => {
-            handleLogin(credentialResponse);
-          }}
-          onError={() => {
-            console.log("Login Failed");
-          }}
-        />
+        {isLoggedIn ? (
+          <div>
+            <button onClick={handleLogout}>Logout</button>
+          </div>
+        ) : (
+          <GoogleLogin
+            onSuccess={(credentialResponse) => {
+              handleLogin(credentialResponse);
+            }}
+            onError={() => {
+              console.log("Login Failed");
+            }}
+          />
+        )}
       </div>
     </div>
   );
