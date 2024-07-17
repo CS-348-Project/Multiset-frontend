9f641d9d8ba9e2fc0e47fd2a88dcdb3afba884a6
Emma Huang <emmahuang.ty@gmail.com>
2024-07-17 16:55:01 -0400

    Improve purchase form

--- src/components/purchase/PurchaseForm.tsx
+++ src/components/purchase/PurchaseForm.tsx
@@ -27,14 +27,14 @@ const FormSchema = z.object({
   category: z.string(),
   total_cost: z.coerce
     .number()
+    .refine((value) => value > 0, { message: "Amount must be greater than 0" })
     .refine(
       (value) => {
         const decimalPlaces = value.toString().split(".")[1]?.length || 0;
         return decimalPlaces <= 2;
       },
       { message: "Maximum of 2 decimal places allowed" }
-    )
-    .refine((value) => value > 0, { message: "Amount must be greater than 0" }),
+    ),
   purchase_splits: z.array(
     z.object({
       borrower: z.number(),
