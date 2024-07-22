466a327e025ab7969e3020ea346ebe69dcad45ef
Emma Huang <emmahuang.ty@gmail.com>
2024-07-22 12:42:42 -0400

    Remove 0 splits

--- src/components/purchase/PurchaseForm.tsx
+++ src/components/purchase/PurchaseForm.tsx
@@ -112,10 +112,12 @@ export function PurchaseForm({
     if (purchase_id !== undefined) {
       data = {
         ...data,
-        purchase_splits: data.purchase_splits.map((split: any) => ({
-          ...split,
-          amount: split.amount ? dollarsToCents(split.amount) : 0,
-        })),
+        purchase_splits: data.purchase_splits
+          .filter((split: any) => split.amount > 0)
+          .map((split: any) => ({
+            ...split,
+            amount: split.amount ? dollarsToCents(split.amount) : 0,
+          })),
         total_cost: data.total_cost ? dollarsToCents(data.total_cost) : 0,
         purchase_id: purchase_id,
       };
@@ -139,10 +141,12 @@ export function PurchaseForm({
     } else {
       data = {
         ...data,
-        purchase_splits: data.purchase_splits.map((split: any) => ({
-          ...split,
-          amount: split.amount ? dollarsToCents(split.amount) : 0,
-        })),
+        purchase_splits: data.purchase_splits
+          .filter((split: any) => split.amount > 0)
+          .map((split: any) => ({
+            ...split,
+            amount: split.amount ? dollarsToCents(split.amount) : 0,
+          })),
         total_cost: data.total_cost ? dollarsToCents(data.total_cost) : 0,
         group_id: group_id,
       };
